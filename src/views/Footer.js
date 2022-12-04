7cea6783c3020fcafc155a6c414a134c87930cbc
Katarzyna Wasilewska <kwasilewska2014@gmail.com>
2022-12-04 17:48:55 +0100

    Add new

---
+++ src/views/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const Footer = () => {
+    return (
+
+        <Container className='text-center text-muted align-content-center mt-5 mb-5'>
+            Copyright @ Waiter-App 2022
+        </Container>
+    )
+}
+export default Footer
