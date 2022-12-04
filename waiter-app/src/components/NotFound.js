8d0de629bed2d3735ad41e5a4638233531b238cc
Katarzyna Wasilewska <kwasilewska2014@gmail.com>
2022-12-04 17:25:22 +0100

    Add files

---
+++ waiter-app/src/components/NotFound.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import TableForm from '../views/TableForm';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h2>
+                Error 404 !
+            </h2>
+        </div>
+    )
+}
+
+export default NotFound;
