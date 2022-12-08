439e300d6fa2693a414f7001acbc76f6e4db3b84
Katarzyna Wasilewska <kwasilewska2014@gmail.com>
2022-12-08 18:42:56 +0100

    add changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 import { fetchTables } from "../redux/tablesRedux";
 import { Row, Col } from 'react-bootstrap';
 import { getAllTables} from '../redux/tablesRedux';
-import List from "../views/List.js.js";
+import List from "../views/List.js";
 
 const Home = () => {
     const dispatch = useDispatch();
